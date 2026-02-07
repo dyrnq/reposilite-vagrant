5aa4350ab2ec151c199d32336f8f99abb4331e23
dyrnq <dyrnq@qq.com>
2026-02-07 21:16:49 +0800

    Update install-java-maven.sh

--- scripts/install-java-maven.sh
+++ scripts/install-java-maven.sh
@@ -5,9 +5,18 @@ mkdir -p ~/.local/jvm/21
 mkdir -p ~/.local/maven/
 mkdir -p ~/.local/bin/
 
+jdk_url="https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.9%2B10/OpenJDK21U-jdk_x64_linux_hotspot_21.0.9_10.tar.gz"
+mvn_url="https://archive.apache.org/dist/maven/maven-3/3.9.12/binaries/apache-maven-3.9.12-bin.tar.gz"
 
-curl -fS#L https://mirrors.tuna.tsinghua.edu.cn/Adoptium/21/jdk/x64/linux/OpenJDK21U-jdk_x64_linux_hotspot_21.0.10_7.tar.gz | tar -xvz --strip-components 1 -C ~/.local/jvm/21
-curl -fs#L https://mirrors.tuna.tsinghua.edu.cn/apache/maven/maven-3/3.9.12/binaries/apache-maven-3.9.12-bin.tar.gz |tar -xvz --strip-components 1 -C ~/.local/maven
+
+# jdk_url="https://mirrors.tuna.tsinghua.edu.cn/Adoptium/21/jdk/x64/linux/OpenJDK21U-jdk_x64_linux_hotspot_21.0.10_7.tar.gz"
+# mvn_url="https://mirrors.tuna.tsinghua.edu.cn/apache/maven/maven-3/3.9.12/binaries/apache-maven-3.9.12-bin.tar.gz"
+
+jdk_url="https://mirrors.ustc.edu.cn/adoptium/releases/temurin21-binaries/jdk-21.0.9%2B10/OpenJDK21U-jdk_x64_linux_hotspot_21.0.9_10.tar.gz"
+mvn_url="https://mirrors.ustc.edu.cn/apache/maven/maven-3/3.9.12/binaries/apache-maven-3.9.12-bin.tar.gz"
+
+curl -fS#L "${jdk_url}" | tar -xvz --strip-components 1 -C ~/.local/jvm/21
+curl -fs#L "${mvn_url}" | tar -xvz --strip-components 1 -C ~/.local/maven
 
 if ! cat ~/.bashrc |grep JAVA_HOME; then
     echo "JAVA_HOME=\$HOME/.local/jvm/21" >> ~/.bashrc
